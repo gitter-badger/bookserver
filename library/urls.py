fb7ea2c8027ebdc8bd8e854031f0bd23c4ed505c
David Wright <discipleofhoid@gmail.com>
2016-01-07 19:32:59 +0000

    add import for templateview

--- library/urls.py
+++ library/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from library.views import BookList, BookDetail, AuthorList, AuthorDetail, Catalog, Index, BookishLogin, BookishUpload 
 
 
