62ae9554759cbdefbb7d14785f12ed097942c365
David Wright <discipleofhoid@gmail.com>
2016-01-08 22:18:03 +0000

    Dont need to split an array

--- library/static/library/public/views/webServices.js
+++ library/static/library/public/views/webServices.js
@@ -8,7 +8,7 @@ angular.module('takeWing').factory('webServices',['$http',function($http){
 			
 			getAutocomplete : function(query){
                 return  $http.get('/shelves/auto/?s='+query).then(function(response){ //wrap it inside another promise using then
-                            return response.data.result_list.split(/, +/g).map( function (result) {
+                            return response.data.result_list.map( function (result) {
                                 return {
                                   value: result,
                                   display: result
