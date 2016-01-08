20c3c92e1abc23c7f5972fe6f9b5cc16063b5f6c
David Wright <discipleofhoid@gmail.com>
2016-01-08 20:41:32 +0000

    fix issues with lookup

--- library/views.py
+++ library/views.py
@@ -195,15 +195,15 @@ class Autocomplete(View):
                 else: 
 				    result_list =['<random> %d' %total_items]
             elif (search_words[0] in "<Author>"):
-                result_list = ["<Author> %s" %author.name for author in Author.objects.filter(__name=filter_search)[:4]]
+                result_list = ["<Author> %s" %author.name for author in Author.objects.filter(name__startswith=filter_search)[:4]]
             elif (search_words[0] in "<Title>"):
-                result_list = ["<Title> %s" %book.title for book in Book.objects.filter(title=filter_search)[:4]]
+                result_list = ["<Title> %s" %book.title for book in Book.objects.filter(title__startswith=filter_search)[:4]]
             elif (search_words[0] in "<Series>"):
-                result_list = ["<Series> %s"%series.name for series in series.objects.filter(name=filter_search)[:4]]
+                result_list = ["<Series> %s"%series.name for series in series.objects.filter(name__startswith=filter_search)[:4]]
         if not result_list:
-            result_list = [author.name for author in Author.objects.filter(name=search_term)[:1]] + \
-		                  [book.title for book in Book.objects.filter(title=search_term)[:1]] + \
-				    	  [series.name for series in Series.objects.filter(name=search_term)[:1]]
+            result_list = [author.name for author in Author.objects.filter(name__startswith=search_term)[:1]] + \
+		                  [book.title for book in Book.objects.filter(title__startswith=search_term)[:1]] + \
+				    	  [series.name for series in Series.objects.filter(name__startswith=search_term)[:1]]
         serialized_data = json.dumps({"result_list":result_list,'debug':{'search_term':search_term,'search_words':search_words}})
         return HttpResponse(serialized_data, content_type="application/json")    
     
