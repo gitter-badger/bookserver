99cba41808fc8c78ada6df041d91438c5bbe78cd
David Wright <discipleofhoid@gmail.com>
2016-01-08 21:26:14 +0000

    Remove unneded code

--- library/static/library/public/views/landingCtrl.js
+++ library/static/library/public/views/landingCtrl.js
@@ -50,24 +50,7 @@
       $log.info('Item changed to ' + JSON.stringify(item));
 	  searchCatalog(item);
     }
-    /**
-     * Build `states` list of key/value pairs
-     */
-    function loadAll() {
-      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-      return allStates.split(/, +/g).map( function (state) {
-        return {
-          value: state.toLowerCase(),
-          display: state
-        };
-      });
-    }
+
     /**
      * Create filter function for a query string
      */
