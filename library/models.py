3e1e43a2757f4247f5d623dd526d3b2738f11a59
David Wright <discipleofhoid@gmail.com>
2016-01-09 04:18:00 +0000

    Wrong fieldname

--- library/models.py
+++ library/models.py
@@ -32,7 +32,7 @@ class Book(models.Model):
         return self.title
     def as_dict(self):
         series_data = [{'id':book_series.series.id,'authors':[model_to_dict(author) for author in book_series.series.authors.all()], \
-                        'summary':book_series.series.summary,'position':book_series.position} for book_series in self.book_series_set.all()]
+                        'summary':book_series.series.summary,'position':book_series.bookNumber} for book_series in self.book_series_set.all()]
         files = [{'name':file.fileType.name, 'description':file.fileType.description,'location':file.fileLocation} for file in self.bookfile_set.all()]
         return {
             'id':self.id,
