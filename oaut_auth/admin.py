6706222d1cbc7384f5f2a17a8e435a04ebb8b55e
David Wright <discipleofhoid@gmail.com>
2016-02-17 21:47:39 +0000

    Remove incorrect admin.py

--- oaut_auth/admin.py
+++ oaut_auth/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from library.models import *
+from .models import *
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Publisher)
-admin.site.register(Series)
-admin.site.register(Tag)
-admin.site.register(FileType)
-admin.site.register(BookFile)
+admin.site.register(FlowModel)
+admin.site.register(CredentialsModel)
