5a6564f83450fd984dfd71832089911c2c0e02ec
David Wright <discipleofhoid@gmail.com>
2016-02-19 00:25:55 +0000

    Add needed imports

--- library/views.py
+++ library/views.py
@@ -19,6 +19,7 @@ from django.views.generic import View
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from itertools import chain
 from library.models import Book, Author, Series, BookFile,Bookish
 from library.myUtils import extract_form_fields
