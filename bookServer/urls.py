1bbb0503609dafcbe1c4b97aa8536342c02712e9
David Wright <discipleofhoid@gmail.com>
2016-02-19 00:24:56 +0000

    Url bug

--- bookServer/urls.py
+++ bookServer/urls.py
@@ -10,9 +10,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bookServer.views.home', name='home'),
     # url(r'^bookServer/', include('bookServer.foo.urls')),
-    url(r'^shelves/', include('library.urls')),
+    url(r'^shelves/', include('library.urls',namespace="library")),
     url(r'^o2/', include('oaut_auth.urls', namespace="oauth2")),
-    url(r'^$', lambda x: HttpResponseRedirect('/shelves/', )namespace="library"),
+    url(r'^$', lambda x: HttpResponseRedirect('/shelves/')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
